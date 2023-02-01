4f5d073dd7ee93ef8a7173a6b87f927c9bbd8a48
Paweł Ciszewski <kardahim@gmail.com>
2023-02-01 19:50:00 +0100

    add background

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
+import './styles/app.scss';
+import bgTopImage from './assets/images/bg-top.svg';
+import bgBottomImage from './assets/images/bg-bottom.svg';
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <main className="app">
+      <img src={bgTopImage} alt="background top" className="app__image app__image--top" />
+      <img src={bgBottomImage} alt="background bottom" className="app__image app__image--bottom" />
+      <h1 className="app__header">Our Pricing</h1>
+      <div className="app__toggle">
+        <span></span>
+        <input type="checkbox" />
+        <span></span>
+      </div>
+      <div className="app__cards"></div>
+      <footer className="app__footer"></footer>
+    </main>
   );
 }
 
